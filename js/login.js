c125c1e6de38c9b5d364a5e99ff581b054d802d2
danilo <danilo.l.rezende@viannasempre.com.br>
2025-08-08 20:28:04 -0300

    inicio js login2

--- js/login.js
+++ js/login.js
@@ -1,9 +1,21 @@
 document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Evita o envio padrão
+
     const usuario = document.getElementById("login").value.trim();
     const senha = document.getElementById("senha").value.trim();
 
+    const usuarioPadrao = "admin@admin";
+    const senhaPadrao = "123456";
+
     if (usuario === "" || senha === "") {
         alert("⚠️ Os campos de usuário e senha não podem ficar vazios!");
-        event.preventDefault(); 
+        return;
+    }
+
+    if (usuario === usuarioPadrao && senha === senhaPadrao) {
+        alert("✅ Login feito com sucesso!");
+        window.location.href = "index.html";
+    } else {
+        alert("❌ Usuário ou senha inválidos!");
     }
 });
